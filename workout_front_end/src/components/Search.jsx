332b04a65805e759e9484c1cbc686aa06c3a6ee7
daleherman03 <daherman66@yahoo.com>
2023-04-23 23:06:07 -0500

    Project-004: Integrated first API

--- workout_front_end/src/components/Search.jsx
+++ workout_front_end/src/components/Search.jsx
@@ -1,7 +1,82 @@
+import { useContext, useState } from "react";
+import { searchByName, searchByMuscle } from "../utilities";
+
 export const Search = () => {
+    const [searchName, setSearchName] = useState("");
+    const [searchMuscle, setSearchMuscle] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+
+    const handleSearchByName = async (e) => {
+        e.preventDefault();
+        const results = await searchByName(searchName);
+        setSearchResults(results);
+        setSearchName("");
+        setSearchMuscle("");
+    };
+
+    const handleSearchByMuscle = async (e) => {
+        e.preventDefault();
+        const results = await searchByMuscle(searchMuscle);
+        setSearchResults(results);
+        console.log(results);
+        setSearchName("");
+        setSearchMuscle("");
+      };
+
     return (
         <>
-        <h1>Search Page</h1>
+            <h3>Search Exercise by Exercise Name</h3>
+            <form onSubmit={handleSearchByName}>
+                <input
+                    placeholder="Enter Exercise Name"
+                    value={searchName}
+                    onChange={(e) => setSearchName(e.target.value)}
+                />
+                <input type="submit" value="Search" />
+            </form>
+
+            <h3>Search Exercise by Target Muscle</h3>
+            <h5>List of Target Muscles</h5>
+            <ul>
+                <li>abductors</li>
+                <li>abs</li>
+                <li>adductors</li>
+                <li>biceps</li>
+                <li>calves</li>
+                <li>cardiovascular system</li>
+                <li>delts</li>
+                <li>forearms</li>
+                <li>glutes</li>
+                <li>hamstrings</li>
+                <li>lats</li>
+                <li>levator scapulae</li>
+                <li>pectorals</li>
+                <li>quads</li>
+                <li>serratus anterior</li>
+                <li>spine</li>
+                <li>traps</li>
+                <li>triceps</li>
+                <li>upper back</li>
+            </ul>
+            <form onSubmit={handleSearchByMuscle}>
+                <input
+                    placeholder="Enter Muscle"
+                    value={searchMuscle}
+                    onChange={(e) => setSearchMuscle(e.target.value)}
+                />
+                <input type="submit" value="Search" />
+            </form>
+
+            {searchResults.length > 0 && (
+                <>
+                    <h3>Search Results</h3>
+                    <ul>
+                        {searchResults.map((result) => (
+                            <li key={result.id}>{result.name}</li>
+                        ))}
+                    </ul>
+                </>
+            )}
         </>
     );
 };
--- workout_front_end/src/utilities.jsx
+++ workout_front_end/src/utilities.jsx
@@ -30,5 +30,44 @@ export const logOut = async(setUser) => {
     let response = await axios.post('/user/logout/')
     if(response.data.logout){
         setUser(null)
-    }
+    };
 }
+
+export const searchByName = async(name) => {
+    const options = {
+        method: 'GET',
+        url: `https://exercisedb.p.rapidapi.com/exercises/name/${name}`,
+        headers: {
+          'content-type': 'application/octet-stream',
+          'X-RapidAPI-Key': '8a3fedd696mshebdd267afb94940p16fa4ajsn9bad1d29ee2d',
+          'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+        }
+      };
+      
+      try {
+          const response = await axios.request(options);
+          return response.data;
+      } catch (error) {
+          console.error(error);
+      };
+};
+
+export const searchByMuscle = async(muscle) => {
+    const options = {
+        method: 'GET',
+        url: `https://exercisedb.p.rapidapi.com/exercises/target/${muscle}`,
+        headers: {
+          'content-type': 'application/octet-stream',
+          'X-RapidAPI-Key': '8a3fedd696mshebdd267afb94940p16fa4ajsn9bad1d29ee2d',
+          'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+        }
+      };
+      
+      try {
+          const response = await axios.request(options);
+          return response.data;
+      } catch (error) {
+          console.error(error);
+      }
+      
+};
